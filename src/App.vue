8dc4dda37b3e396748983b5a194b010d40e13770
TerenceGrover <terencegrover@gmail.com>
2023-08-21 20:40:54 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@ import ConfettiExplosion from "vue-confetti-explosion";
 import { initializeApp } from "firebase/app";
 // forebase config from env
 
-console.log('Environment variable:', process.env.firebaseConfig);
+console.log('Environment variable:', process.env);
+console.log('Environment variable:', meta.env);
 const firebaseConfig = JSON.parse(process.env.firebaseConfig);
 
 
